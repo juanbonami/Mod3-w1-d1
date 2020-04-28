1532474b12540765382d9bc47bc2c3a875fff523
Juan Bonami <Juanbonami@gmail.com>
2020-04-28 18:08:46 -0500

    Imported the Scanner class from the java.util to use for user input. We store the user input into a int variable called userInput from in.nextInt() method.

--- Week3HW.java
+++ Week3HW.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Week3HW {
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        int userInput = in.nextInt();
+
     }
 }
