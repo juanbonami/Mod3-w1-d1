5c4552ba394e68837128ebfe373f3875ef3deb4f
Juan Bonami <Juanbonami@gmail.com>
2020-04-28 19:12:02 -0500

    added some print methods to direct the user

--- Week3HW.java
+++ Week3HW.java
@@ -7,18 +7,20 @@ public class Week3HW {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter number: ");
-
         int firstUserInput = in.nextInt();
+
+        System.out.print("Enter number: ");
         int secondUserInput = in.nextInt();
 
-        System.out.println(returnUserInputs(firstUserInput, secondUserInput));
+        System.out.println("Answer: " + returnUserInputs(firstUserInput, secondUserInput));
 
         // Write a Java program that reads a number in inches, converts it to meters.
         // Note: One inch is 0.0254 meter. Example: Input a value for inch: 1000 Expected Output: 1000.0 inch is 25.4 meters
 
+        System.out.print("Enter number in inches: ");
         double inches = in.nextDouble();
 
-        System.out.println(inchesToMeters(inches));
+        System.out.println("Answer in meters: " + inchesToMeters(inches));
 
 
     }
